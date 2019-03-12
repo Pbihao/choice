b4de33a1ba6529d9a3200a0e6b45926b38911b71
Pbihao <1435343052@qq.com>
2019-03-12 21:32:35 +0800

    建立与服务器连接

--- miniprogram/pages/luck/luck.js
+++ miniprogram/pages/luck/luck.js
@@ -31,18 +31,23 @@ Page({
         _openid: app.globalData.openid
       }).count()
       const total = countResult.total
-      const batchTimes = Math.ceil(total / 2)
+      const batchTimes = Math.ceil(total / 20)
       var cards = []
       for (let i = 0; i < batchTimes; i++) {
           const promise = collection.where({
                         _openid: app.globalData.openid
-                        }).skip(i * 2).limit(2).get()
+                        }).skip(i * 20).limit(20).get()
           cards.push(promise)
       }
-      var a = (await Promise.all(cards)).reduce((acc, cur) => ({
-        data: acc.data.concat(cur.data),
-        errMsg: acc.errMsg,
-      }))
+      var a = {}
+      if(batchTimes >= 1){
+        a = (await Promise.all(cards)).reduce((acc, cur) => ({
+          data: acc.data.concat(cur.data),
+          errMsg: acc.errMsg,
+        }))
+      }else {
+        a.data = []
+      }
       that.setData({
         myChoices: a.data
       })
@@ -51,59 +56,35 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
     this.load()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //用户删除一张卡片
+  ondelete: async function(e){
+    let index = e.currentTarget.dataset.index;
+    var that = this
+    console.log(that.data.myChoices[index])
+    const collection = await wx.cloud.database().collection('user_cards')
+    await collection.doc(that.data.myChoices[index]._id).remove().then(() => {
+      that.data.myChoices.splice(index, 1)
+      wx.setStorageSync('myChoices_list', that.data.myChoices)
+      console.log("删除成功")
+      that.load()
+    }).catch((res)=>{
+      console.log(res)
+      wx.showToast({
+        title: '删除失败请重试',
+        duration: 1000,
+        icon: 'none'
+      })
+    })
   },
 
   //跳转添加choice页面
   //如果没有获取openid就提示没有登陆
   creatChoice: function () {
-
     wx.setStorageSync('edit',0)
     app.getOpenid().then(() => {
       wx.navigateTo({
@@ -118,11 +99,15 @@ Page({
 
     })
   },
+
+
   choose: function() {
     wx.navigateTo({
       url: '/pages/luck/choose/choose',
     })   
   },
+
+
   edit: function(e) {
     let index = e.currentTarget.dataset.index;
     wx.setStorageSync('edit',index)
