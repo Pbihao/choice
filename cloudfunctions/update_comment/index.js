70813c5bfaa63e2d2c7c8811bf5c473ae56446f7
L-M-Sherlock <l.m.kagamigawachloe@outlook.com>
2019-03-19 19:18:30 +0800

    评论

---
+++ cloudfunctions/update_comment/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db=cloud.database()
+cloud.init()
+const _=db.command
+const col=db.collection("questions")
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    return await col.doc(id).update({
+      data: {
+        comment_number: _.inc(1),
+        comment: _.push(event.comment)
+      }
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
