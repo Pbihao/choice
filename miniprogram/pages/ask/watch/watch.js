b661865a54d83bbaf45d5c26569d11ccaab79c97
L-M-Sherlock <l.m.kagamigawachloe@outlook.com>
2019-03-19 23:36:54 +0800

    评论区重构

--- miniprogram/pages/ask/watch/watch.js
+++ miniprogram/pages/ask/watch/watch.js
@@ -7,18 +7,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-    ask: null,
-    comment_detail: ""
+    detail:'',
+    avatar_name:'',
+    date:null,
+    avatar_url:'',
+    comment:null,
+    comment_number:0,
+    comment_detail: '',
+    _id:null,
+    img_path: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      ask: wx.getStorageSync('ask')
-    })
-    console.log(this.data.ask)
   },
 
   /**
@@ -32,7 +35,18 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    var ask = wx.getStorageSync('ask')
 
+    this.setData({
+      comment: ask.comment,
+      comment_number: ask.comment.length,
+      avatar_name: ask.avatar_name,
+      avatar_url: ask.avatar_url,
+      date: ask.date,
+      detail: ask.detail,
+      _id: ask._id,
+      img_path: ask.img_path
+    })
   },
 
   /**
@@ -70,9 +84,10 @@ Page({
 
   },
   comment_detail: function (e) {
+    console.log('3')
     console.log(e.detail)
     this.setData({
-      comment_detail: e.detail
+      comment_detail: e.detail.value
     })
   },
   submit: function () {
@@ -90,15 +105,16 @@ Page({
       user_avatar: app.globalData.avatarUrl,
       user_name: app.globalData.nickName
     }
-    this.data.ask.comment.push(comment)
+    this.data.comment.push(comment)
     this.setData({
-      comment_detail: '',
-      ask: this.data.ask
+      comment_detail:'',
+      comment: this.data.comment,
+      comment_number: this.data.comment_number + 1
     })
     wx.cloud.callFunction({
       name: 'update_comment',
       data: {
-        id: this.data.ask._id,
+        id: this.data._id,
         comment: comment
       },
       success: res => {
