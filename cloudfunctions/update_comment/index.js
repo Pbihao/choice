01205399b7bcece83fc028583840d952adc7fcb5
Pbihao <1435343052@qq.com>
2019-03-19 19:27:00 +0800

    合并冲突

--- cloudfunctions/update_comment/index.js
+++ cloudfunctions/update_comment/index.js
@@ -8,7 +8,7 @@ const col=db.collection("questions")
 // 云函数入口函数
 exports.main = async (event, context) => {
   try {
-    return await col.doc(id).update({
+    return await col.doc(event.id).update({
       data: {
         comment_number: _.inc(1),
         comment: _.push(event.comment)
