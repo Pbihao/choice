5bcda4a1bb7cbe9981949c6bd597d6e6f2005446
Pbihao <1435343052@qq.com>
2019-03-22 21:22:37 +0800

    修复bug

--- miniprogram/pages/mine/my_ask/my_ask.js
+++ miniprogram/pages/mine/my_ask/my_ask.js
@@ -2,24 +2,29 @@ const app = getApp()
 import regeneratorRuntime from '../../../regenerator-runtime/runtime.js';
 Page({
   data: {
-    hidden: true,
+    hidden: false,
     content: [],
     length: 0,
     total: null,
+    last_date: null,
     begin: null,
     allin: false,//数据库中所有的卡片都已经被加载了
     now_unique: -1,
-    refresh: false
+    refresh: false,
+    first_log: true
   },
   //点击加入新的问问
   add_new_ask: function () {
     if (!app.globalData.userInfo) {
-      wx.showToast({
-        title: "未登陆",
-        duration: 1000
-      })
-      wx.switchTab({
-        url: '../mine/mine',
+      setTimeout(() => {
+        wx.showToast({
+          title: "未登陆",
+          icon: "none",
+          duration: 300
+        })
+      }, 1000)
+      wx.navigateTo({
+        url: '../mine',
       })
       return
     } else {
@@ -27,7 +32,7 @@ Page({
     }
     console.log("发布新的问问")
     wx.navigateTo({
-      url: './edit/edit',
+      url: '../../ask/edit/edit',
     })
   },
   //点击左边的按钮
@@ -88,10 +93,6 @@ Page({
       }
     })
   },
-  //点击了卡片跳转
-  click_card: function () {
-    console.log("跳转")
-  },
   //点击了查看图片
   see_img: function (e) {
     var that = this
@@ -101,53 +102,127 @@ Page({
       urls: [that.data.content[id].img_path]
     })
   },
+
+  /*
   //加载新的卡片,每次十张
-  load_card: function () {
-    if (this.data.allin) return
+  load_card: function(){
+    if(this.data.allin)return
     var lt = 10,
-      begin = this.data.begin,
-      that = this
+        begin = this.data.begin,
+        that = this
     begin -= lt
-    if (begin < 0) {
-      this.data.allin = true
+    if(begin < 0){
+      this.data.allin=true
       lt += begin
       begin = 0
     }
-    this.data.begin = begin
+    this.data.begin=begin
     const col = wx.cloud.database().collection('questions')
-    console.log("从哪里开始", begin)
-    console.log("加载多少个", lt)
+    console.log("从哪里开始",begin)
+    console.log("加载多少个",lt)
     col.skip(begin)
-      .limit(lt)
-      .where({
-        _openid:app.globalData.openid
-      })
-      .orderBy('date', 'desc')
-      .get()
-      .then(res => {
-        console.log(res.data)
-        var cont = that.data.content,
-          i
-        for (i of res.data) {
-          that.data.now_unique += 1
-          i.unique = that.data.now_unique
-          //加载的时候之前就已经投过票了
-          if (i.used.includes(app.globalData.openid)) {
-            i.left_txt = i.left + '票'
-            i.right_txt = i.right + '票'
-            i.hased = true;
-          } else {
-            i.hased = false
+       .limit(lt)
+       .orderBy('date', 'desc')
+        .get()
+        .then(res=>{
+          console.log(res.data)
+          var cont = that.data.content,
+              i
+          for(i of res.data){
+            that.data.now_unique+=1
+            i.unique=that.data.now_unique
+            //加载的时候之前就已经投过票了
+            if(i.used.includes(app.globalData.openid)){
+              i.left_txt = i.left + '票'
+              i.right_txt = i.right + '票'
+              i.hased=true;
+            }else{
+              i.hased=false
+            }
+            cont.push(i)
           }
-          cont.push(i)
-        }
-        that.setData({
-          content: cont
+          that.setData({
+            content: cont
+          })
+          if(that.data.first_log){
+            that.data.first_log=false
+            that.setData({
+              first_log: false,
+              hidden: true
+            })
+          }
+        }).catch(err=>{
+          console.error("获取记录失败：", err)
         })
-      }).catch(err => {
-        console.error("获取记录失败：", err)
-      })
   },
+  */
+  //加载卡片 一次十张
+  load_card: function () {
+    var lt = 10,
+      that = this
+    const col = wx.cloud.database().collection('questions')
+    var that = this
+    new Promise((resolve, reject) => {
+      wx.cloud.callFunction({
+        name: 'get_cards_number',
+        data: {},
+        success: res => {
+          console.log("数据库中的记录个数：", res.result.total)
+          that.data.last_total = that.data.total
+          that.data.total = res.result.total
+          resolve()
+        },
+        fail: err => {
+          console.error("调用失败:", err)
+        }
+      })
+    }).then(res => {
+      var begin = that.data.begin
+      begin += that.data.total - that.data.last_total
+      console.log("从哪里开始", begin)
+      console.log("上一次的总数", that.data.last_total)
+      console.log("这一次的总数", that.data.total)
+      if (begin >= that.data.total) return
+      col.skip(begin)
+        .where({
+          _openid: app.globalData.openid
+        })
+        .limit(lt)
+        .orderBy('date', 'desc')
+        .get()
+        .then(res => {
+          console.log(res.data)
+          var cont = that.data.content, i
+          for (i of res.data) {
+            that.data.now_unique += 1
+            i.unique = that.data.now_unique
+            //加载的时候之前就已经投过票了
+            if (i.used.includes(app.globalData.openid)) {
+              i.left_txt = i.left + '票'
+              i.right_txt = i.right + '票'
+              i.hased = true;
+            } else {
+              i.hased = false
+            }
+            cont.push(i)
+          }
+          that.setData({
+            content: cont
+          })
+          if (that.data.first_log) {
+            that.data.first_log = false
+            that.setData({
+              first_log: false,
+              hidden: true
+            })
+          }
+        }).catch(err => {
+          console.error("获取记录失败：", err)
+        })
+      that.data.begin += lt
+    })
+  },
+
   //第一次打开界面 获得加载问问的起点
   onLoad: async function () {
     let scrollHeight = wx.getSystemInfoSync().windowHeight;
@@ -161,7 +236,7 @@ Page({
         console.log("数据库中的记录个数：", res.result.total)
         this.setData({
           total: res.result.total,
-          begin: res.result.total
+          begin: 0
         })
         this.load_card()
       },
@@ -169,9 +244,6 @@ Page({
         console.error("调用失败:", err)
       }
     })
-    if (!app.globalData.openid) {
-      await app.getOpenid()
-    }
   },
   onShow: async function () {
     var that = this
@@ -188,8 +260,8 @@ Page({
         success: res => {
           console.log("数据库中的记录个数：", res.result.total)
           that.setData({
-            total: res.result.total,
-            begin: res.result.total
+            last_total: res.result.total,
+            begin: 0
           })
           that.load_card()
         },
@@ -197,9 +269,24 @@ Page({
           console.error("调用失败:", err)
         }
       })
-      if (!app.globalData.openid) {
-        await app.getOpenid()
-      }
     }
+  },
+  //点击了卡片跳转
+  comment: function (e) {
+    console.log("跳转卡片")
+    wx.setStorageSync('ask', e.currentTarget.dataset.ask)
+    wx.navigateTo({
+      url: '../../ask/watch/watch',
+    })
+  },
+  onPullDownRefresh: function () {
+    this.onLoad
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.onLoad
   }
 })
